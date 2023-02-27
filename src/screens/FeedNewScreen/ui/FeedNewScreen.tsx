341623921b7f86497c63e1ad14daea48adaf178d
Александр Субботин <>
2023-02-27 20:10:29 +0300

    fix: minnor

--- src/screens/FeedNewScreen/ui/FeedNewScreen.tsx
+++ src/screens/FeedNewScreen/ui/FeedNewScreen.tsx
@@ -1,10 +1,14 @@
+import {setScrolledOnBottomTab} from '@shared/lib/setScrolledOnBottomTab';
+import {$scrollValue} from '@shared/model/scrollingContentOnBottomTabs';
 import {Calendar} from '@shared/ui/organisms/Calendar';
 import {ScreenTemplate} from '@shared/ui/templates/ScreenTemplate';
 import dayjs from 'dayjs';
+import {useStore} from 'effector-react';
 import React, {useState} from 'react';
 
 export const FeedNewScreen: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState(dayjs());
+  const scrollValue = useStore($scrollValue);
   return (
     <ScreenTemplate
       scrollProps={{
