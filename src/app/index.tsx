504a3b386ce6103d5faf26b5f54ed573da1adf2b
Александр Субботин <>
2023-02-27 19:00:36 +0300

    fix: added init for  i18n

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,12 @@
-import {NavigationContainer} from '@react-navigation/native';
-import i18next from '@shared/config/locales/i18n';
+import {initI18next} from '@shared/config/locales/i18n';
+
 import {
   $currentSafeAreaColor,
   $isBottomBar,
 } from '@shared/model/currentSafeAreaColor';
 import {withProviders} from '@shared/providers/withNavigation';
 import {useStore} from 'effector-react';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {MainStack} from './navigation';
 import {useStyles} from './styles';
@@ -14,7 +14,11 @@ import {useStyles} from './styles';
 const App: React.FC = () => {
   const safeAreaBackgroundColor = useStore($currentSafeAreaColor);
   const isBottomBar = useStore($isBottomBar);
-  i18next.t('common.email');
+
+  useEffect(() => {
+    initI18next();
+  }, []);
+
   const styles = useStyles();
   return (
     <>
--- src/shared/config/locales/i18n.ts
+++ src/shared/config/locales/i18n.ts
@@ -1,24 +1,19 @@
-import i18next, {i18n} from 'i18next';
-import {initReactI18next, TFunction} from 'react-i18next';
+import i18next from 'i18next';
+import {initReactI18next} from 'react-i18next';
 import EN from './locales/en-us.json';
 
-const resources = {
+export const resources = {
   en: {
     translation: EN,
   },
 };
 
-i18next.use(initReactI18next).init({
-  compatibilityJSON: 'v3',
-  lng: 'en',
-  interpolation: {
-    escapeValue: false,
-  },
-  resources,
-});
-
-export interface Customi18n extends i18n {
-  t: TFunction;
-}
-
-export default i18next as Customi18n;
+export const initI18next = () =>
+  i18next.use(initReactI18next).init({
+    compatibilityJSON: 'v3',
+    lng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+    resources,
+  });
