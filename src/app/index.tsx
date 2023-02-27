0c3b6499f2465bde24fa3f3c332b76ce349f7912
Александр Субботин <>
2023-02-27 19:25:01 +0300

    Revert "fix: added init for  i18n"
    This reverts commit 504a3b386ce6103d5faf26b5f54ed573da1adf2b.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,12 @@
-import {initI18next} from '@shared/config/locales/i18n';
-
+import {NavigationContainer} from '@react-navigation/native';
+import i18next from '@shared/config/locales/i18n';
 import {
   $currentSafeAreaColor,
   $isBottomBar,
 } from '@shared/model/currentSafeAreaColor';
 import {withProviders} from '@shared/providers/withNavigation';
 import {useStore} from 'effector-react';
-import React, {useEffect} from 'react';
+import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {MainStack} from './navigation';
 import {useStyles} from './styles';
@@ -14,11 +14,7 @@ import {useStyles} from './styles';
 const App: React.FC = () => {
   const safeAreaBackgroundColor = useStore($currentSafeAreaColor);
   const isBottomBar = useStore($isBottomBar);
-
-  useEffect(() => {
-    initI18next();
-  }, []);
-
+  i18next.t('common.email');
   const styles = useStyles();
   return (
     <>
--- src/shared/config/locales/i18n.ts
+++ src/shared/config/locales/i18n.ts
@@ -1,19 +1,24 @@
-import i18next from 'i18next';
-import {initReactI18next} from 'react-i18next';
+import i18next, {i18n} from 'i18next';
+import {initReactI18next, TFunction} from 'react-i18next';
 import EN from './locales/en-us.json';
 
-export const resources = {
+const resources = {
   en: {
     translation: EN,
   },
 };
 
-export const initI18next = () =>
-  i18next.use(initReactI18next).init({
-    compatibilityJSON: 'v3',
-    lng: 'en',
-    interpolation: {
-      escapeValue: false,
-    },
-    resources,
-  });
+i18next.use(initReactI18next).init({
+  compatibilityJSON: 'v3',
+  lng: 'en',
+  interpolation: {
+    escapeValue: false,
+  },
+  resources,
+});
+
+export interface Customi18n extends i18n {
+  t: TFunction;
+}
+
+export default i18next as Customi18n;
