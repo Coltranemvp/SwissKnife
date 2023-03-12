7cca15093a776a556118328dbee0bb23254a1463
Александр Субботин <>
2023-03-12 11:36:45 +0300

    feat: added prettier imports settings

--- src/screens/CalendarScreen/ui/index.tsx
+++ src/screens/CalendarScreen/ui/index.tsx
@@ -1,7 +1,10 @@
-import {ScreenTemplate} from '@shared/ui/templates/ScreenTemplate';
-import {Calendar} from '@widgets/Calendar';
-import dayjs from 'dayjs';
 import React, {useState} from 'react';
+
+import dayjs from 'dayjs';
+
+import {Calendar} from '@widgets/Calendar';
+
+import {ScreenTemplate} from '@shared/ui/templates/ScreenTemplate';
 
 export const CalendarScreen: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState(dayjs());
